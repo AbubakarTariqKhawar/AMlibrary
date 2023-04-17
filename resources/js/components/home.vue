01c35e4e8bfaa73d771937bd7a7238fc97c4f753
abubakar <abubakarprova@gmail.com>
2023-04-17 17:56:03 +0200

    adding book

--- resources/js/components/home.vue
+++ resources/js/components/home.vue
@@ -19,6 +19,38 @@
             <h3>Trending Books</h3>
         </div>
     </section>
+    <section>
+        <div class="container ">
+            <div class="row">
+                <div class="card col-12 col-md-3 mt-3 showtext">
+                    <img src="../../books/bookCourt.webp" class="card-img" alt="Book">
+                    <div class=" overlay">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small>Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+
+                <div class="card col-12 col-md-3 mt-3 showtext">
+                    <img src="../../books/bookCourt.webp" class="card-img" alt="Book">
+                    <div class=" overlay">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small>Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+
+                <div class="card col-12 col-md-3 mt-3 showtext">
+                    <img src="../../books/bookCourt.webp" class="card-img" alt="Book">
+                    <div class=" overlay">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small>Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
