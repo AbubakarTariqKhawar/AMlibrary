b0c212c3eeccc742ba946f06736a01097c4a197e
abubakar <abubakarprova@gmail.com>
2023-04-18 19:22:57 +0200

    added stuf

--- resources/js/components/home.vue
+++ resources/js/components/home.vue
@@ -2,28 +2,38 @@
     <section id="banner_principal" class="container-fluid">
             <div class="col-12 banner ">
                 <div class="container d-flex">
-                    <h1 class=" text-color3">CrackPizza All you needa</h1>
+                    <h1 class=" text-color3">The enthusiasm for books</h1>
                 </div>
                 <div class="container d-flex">
-                    <h2 class=" text-color3 ml-4"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; is love and Pizza</h2>
+                    <h2 class=" text-color3 ml-4"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Never stops</h2>
                 </div>
                 <div class="container d-flex">
                     <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <button type="button" onclick="location.href='<?=base_url.'pizza/menu'?>'" class="btn btn-dark">Order Now</button>
+                    <button type="button" onclick="location.href='<?=base_url.'pizza/menu'?>'" class="btn ">Read Now</button>
                     </h2>
                 </div>
             </div>
     </section>
     <section>
-        <div class="container ">
+        <div class="container mt-3 ">
             <h3>Trending Books</h3>
         </div>
     </section>
     <section>
         <div class="container ">
             <div class="row">
-                <div class="cardT col-12 col-md-3 mt-3 showtext">
-                    <img src="../../books/bookCourt.webp"  alt="Book" height="80%" width="90%">
+                <div class="card col-12 col-md-3 mt-3 mb-3  showtext">
+                    <img src="../../books/bookCourt.webp" class="card-img" alt="Book" height="100%" width="90%">
+                    <div class=" overlay">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small>Last updated 3 mins ago</small></p>
+
+                    </div>
+                </div>
+
+                <div class="card col-12 col-md-3 mt-3 mb-3 showtext">
+                    <img src="../../books/bookTeias.webp" class="card-img" alt="Book" height="100%" width="90%">
                     <div class=" overlay">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -31,26 +41,15 @@
                     </div>
                 </div>
 
-                <div class="cardT col-12 col-md-3 mt-3 showtext">
-                    <img src="../../books/bookTeias.webp" alt="Book" height="80%" width="90%">
-                    <div class=" overlay">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small>Last updated 3 mins ago</small></p>
+                <div class="card col-12 col-md-3 mt-3 mb-3 showtext">
+                    <img src="../../books/bookCourt.webp" class="card-img"  alt="Book" height="100%" width="90%">
+                    <div class="overlay">
+                        <div class="text">Hello World</div>
                     </div>
                 </div>
 
-                <div class="cardT col-12 col-md-3 mt-3 showtext">
-                    <img src="../../books/bookCourt.webp"  alt="Book" height="80%" width="90%">
-                    <div class=" overlay">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small>Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-
-                <div class="cardT col-12 col-md-3 mt-3 showtext">
-                    <img src="../../books/bookTeias.webp" alt="Book" height="80%" width="90%">
+                <div class="card col-12 col-md-3 mt-3 mb-3 showtext">
+                    <img src="../../books/bookTeias.webp" class="card-img" alt="Book" height="100%" width="90%">
                     <div class=" overlay">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
