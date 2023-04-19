1a1b5b4d55043fdc1831ae0d8a0db8bebfae10e3
abubakar <abubakarprova@gmail.com>
2023-04-19 20:37:06 +0200

    added category

--- resources/js/components/home.vue
+++ resources/js/components/home.vue
@@ -16,14 +16,14 @@
     </section>
     <section>
         <div class="container mt-3 ">
-            <h3>Trending Books</h3>
+            <h3><b>Trending Books</b></h3>
         </div>
     </section>
     <section>
         <div class="container ">
             <div class="row">
                 <div class="card col-12 col-md-3 mt-3 mb-3  showtext">
-                    <img src="../../books/bookCourt.webp" class="card-img" alt="Book" height="100%" width="90%">
+                    <img src="../../books/undereHerSkin.webp" class="card-img" alt="Book" height="100%" width="90%">
                     <div class=" overlay">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -33,7 +33,7 @@
                 </div>
 
                 <div class="card col-12 col-md-3 mt-3 mb-3 showtext">
-                    <img src="../../books/bookTeias.webp" class="card-img" alt="Book" height="100%" width="90%">
+                    <img src="../../books/rickRiordan.webp" class="card-img" alt="Book" height="100%" width="90%">
                     <div class=" overlay">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -42,14 +42,14 @@
                 </div>
 
                 <div class="card col-12 col-md-3 mt-3 mb-3 showtext">
-                    <img src="../../books/bookCourt.webp" class="card-img"  alt="Book" height="100%" width="90%">
+                    <img src="../../books/horrorbo.webp" class="card-img"  alt="Book" height="100%" width="90%">
                     <div class="overlay">
                         <div class="text">Hello World</div>
                     </div>
                 </div>
 
                 <div class="card col-12 col-md-3 mt-3 mb-3 showtext">
-                    <img src="../../books/bookTeias.webp" class="card-img" alt="Book" height="100%" width="90%">
+                    <img src="../../books/goodwishgonebad.webp" class="card-img" alt="Book" height="100%" width="90%">
                     <div class=" overlay">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -59,6 +59,193 @@
 
             </div>
         </div>
+    </section>
+    <section>
+        <div class="container mt-4 ">
+            <h3><b>Categories</b></h3>
+        </div>
+    </section>
+    <section class="bookA">
+            <div class="component">
+                <ul class="align">
+                    <li>
+                        <figure class='book'>
+
+                            <!-- Front
+
+                            <ul class='hardcover_front'>
+                                <li>
+                                    <div class="coverDesign yellow">
+                                        <span class="ribbon">NEW</span>
+                                        <h1>Horror</h1>
+                                        <p>Books</p>
+                                    </div>
+                                </li>
+                                <li></li>
+                            </ul>
+                            -->
+
+                            <ul class='hardcover_front'>
+                                <li>
+                                    <img src="../../books/horror.webp" alt="" width="100%" height="100%">
+                                </li>
+                                <li></li>
+                            </ul>
+
+                            <!-- Pages -->
+
+                            <ul class='page'>
+                                <li></li>
+                                <li>
+                                    <a class="btn" href="#">Read</a>
+                                </li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+
+                            <!-- Back -->
+
+                            <ul class='hardcover_back'>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                            <ul class='book_spine'>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                            <figcaption>
+                                <h2><b>Horror</b></h2>
+                                <span>The enthusiasm for books..</span>
+                                <p>Here you can read Best horror books ever writen and by reading you can experience horror.</p>
+                            </figcaption>
+                        </figure>
+                    </li>
+                    <li>
+                        <figure class='book'>
+
+                            <!-- Front -->
+
+                            <ul class='hardcover_front'>
+                                <li>
+                                    <img src="../../books/comic.webp" alt="" width="100%" height="100%">
+                                </li>
+                                <li></li>
+                            </ul>
+
+                            <!-- Pages -->
+
+                            <ul class='page'>
+                                <li></li>
+                                <li>
+                                    <a class="btn" href="#">Read</a>
+                                </li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+
+                            <!-- Back -->
+
+                            <ul class='hardcover_back'>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                            <ul class='book_spine'>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                            <figcaption>
+                                <h2><b>Comics</b></h2>
+                                <span>The enthusiasm for books..</span>
+                                <p>Here you can read Best horror books ever writen and by reading you can experience horror.</p>
+                            </figcaption>
+                        </figure>
+                    </li>
+                    <li>
+                        <figure class='book'>
+
+                        <!-- Front -->
+
+                            <ul class='hardcover_front'>
+                                <li>
+                                    <img src="../../books/romentic.webp" alt="" width="100%" height="100%">
+                                </li>
+                                <li></li>
+                            </ul>
+
+                        <!-- Pages -->
+
+                            <ul class='page'>
+                                <li></li>
+                                <li>
+                                    <a class="btn" href="#">Read</a>
+                                </li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+
+                        <!-- Back -->
+
+                            <ul class='hardcover_back'>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                            <ul class='book_spine'>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                            <figcaption>
+                                <h2><b>Romance</b></h2>
+                                <span>The enthusiasm for books..</span>
+                                <p>Here you can read Best horror books ever writen and by reading you can experience horror.</p>
+                            </figcaption>
+                        </figure>
+                    </li>
+                    <li>
+                        <figure class='book'>
+
+                        <!-- Front -->
+
+                            <ul class='hardcover_front'>
+                                <li>
+                                    <img src="../../books/kids.webp" alt="" width="100%" height="100%">
+                                </li>
+                                <li></li>
+                            </ul>
+
+                        <!-- Pages -->
+
+                            <ul class='page'>
+                                <li></li>
+                                <li>
+                                    <a class="btn" href="#">Read</a>
+                                </li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+
+                        <!-- Back -->
+
+                            <ul class='hardcover_back'>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                            <ul class='book_spine'>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                            <figcaption>
+                                <h2><b>Kids</b></h2>
+                                <span>The enthusiasm for books..</span>
+                                <p>Here you can read Best horror books ever writen and by reading you can experience horror.</p>
+                            </figcaption>
+                        </figure>
+                    </li>
+                </ul>
+            </div>
     </section>
 </template>
 
